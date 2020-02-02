6f395c9996c9854193da6a6516f66ae32223e35d
DESKTOP-J4U3P7B\windows <info@ahmadnaser.com>
2020-02-02 22:04:27 +0200

    page templates from plugin
    page templates from plugin

---
+++ anbilarabi-template.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ * Template Name: anbilarabi Download
+ * Description: A Page Template with a darker design.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>تحميل تطبيق أحمد ناصر بالعربي</title>
+    <meta name="description" content="تحميل تطبيق أحمد ناصر بالعربي anbilarabi.com أحدث الدورات والمقالات خصوصمات كبيرة واستفادة عظيمة"/>
+    <link href="https://fonts.googleapis.com/css?family=Dosis:600|Roboto:400,700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo plugins_url('/css/bootstrap.min.css', __FILE__); ?>" rel="stylesheet">
+    <link href="<?php echo plugins_url('/css/paper-kit.css?v=2.1.0', __FILE__); ?>" rel="stylesheet">
+    <link href="<?php echo plugins_url('/styles/main.css?ref2', __FILE__); ?>" rel="stylesheet">
+	<style>
+	img.img-fluid.offfer {
+    position: absolute;
+    width: 185px;
+    bottom: 0px;
+    left: 110px;
+}
+.hidden{
+	display:none;
+}
+img.img-fluid.pr-3.aa-logo-img {
+    background: #ffffffa1;
+    padding: 10px;
+    border-radius: 5px;
+    margin: 15px;
+    border: 1px solid green;
+    min-width: 150px;
+}
+	</style>
+  </head>
+  <body id="top">
+    <header>
+      <div class="aa-header">
+        <nav class="navbar navbar-expand-md navbar-transparent">
+          <div class="container"><img class="img-fluid pr-3 aa-logo-img" width="100" src="<?php echo plugins_url('/images/anbilarabi-vector-1.png', __FILE__); ?>" alt="logo"><a class="navbar-brand px-0 py-0" href="https://anbilarabi.com/">أحمد ناصر بالعربي</a>
+            <div class="collapse navbar-collapse">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="https://anbilarabi.com">الرئيسية</a></li>
+				      <li class="nav-item"><a class="nav-link" href="https://anbilarabi.com/khottah">الخطة الشاملة</a></li>
+                <li class="nav-item"><a class="btn btn-outline-neutral btn-round" href="#download">تحميل الآن</a></li>
+              </ul> 
+            </div>
+          </div>
+        </nav>
+        <div class="container aa-header-content text-left text-white">
+          <div class="row">
+            <div class="col-md-6 col-sm-12">
+              <h2 class="text-white mb-4">Anbilarabi <br/>عالم من البرمجة والتسويق بين يديك</h2>
+              <p>ان بالعربي هو موقع تقني عربي لتعليم البرمجة والتسويق والتجارة الالكترونية احصل على احدث المقالات والدورات من خلال تطبيقنا.</p>
+              <ul class="py-1 list-unstyled">
+                <li class="py-2"><i class="fa fa-check-circle pr-4" aria-hidden="true"></i> أكثر من 1000 مقالة</li>
+                <li class="py-2"><i class="fa fa-check-circle pr-4" aria-hidden="true"></i> دورات تخصصية برمجية</li>
+                <li class="py-2"><i class="fa fa-check-circle pr-4" aria-hidden="true"></i> تحديثات مجانية</li>
+              </ul><a class="mt-4 btn btn-outline-neutral btn-round" href="#download">ابدا الان</a>
+            </div>
+            <div class="col-md-6 col-sm-12 text-right"><h2 class="offfer">خصومات 50% على جميع الدورات</h2><img class="img-fluid" src="<?php echo plugins_url('/images/anbilarabi-app.png', __FILE__); ?>" alt="Image"></div>
+          </div>
+        </div>
+      </div>
+	  <div class="aa-download-section section bg-gradient" id="download" style="padding-top:0px;margin-top: -70px;">         
+  <div class="container">
+    <div class="h2 text-center text-title text-white pb-5">Download anbilarabi App Now </div>
+    <div class="row">
+      <div class="col-md-6 col-sm-12"><a class="aa-apple" href="https://appsto.re/us/5inKcb.i">
+	   <img width="100%" src="https://www.anbilarabi.com/wp-content/uploads/2016/06/Download_on_the_App_Store_Badge.svg_.png" />
+          <div class="card hidden" >
+            <div class="row pb-3">
+              <div class="col-lg-3 col-md-12 aa-download-icon"><i class="pt-4 fa fa-apple fa-4x" aria-hidden="true"></i></div>
+              <div class="col-lg-9 col-md-12 aa-download-icon-detail">
+			 
+                <div class="h4 pb-1">Download</div>
+                <p class="text-muted">Download from App Store</p>
+              </div>
+            </div>
+          </div></a></div>
+      <div class="col-md-6 col-sm-12"><a class="aa-android" href="https://play.google.com/store/apps/details?id=com.ahmadnaser.anbilarabi&hl=ar">
+	  <img width="100%" src="https://www.anbilarabi.com/wp-content/uploads/2016/05/google-play-logo.png" />
+          <div class="card hidden" >
+            <div class="row pb-3">
+              <div class="col-lg-3 col-md-12 aa-download-icon"><i class="pt-4 fa fa-android fa-4x" aria-hidden="true"></i></div>
+              <div class="col-lg-9 col-md-12 aa-download-icon-detail">
+			   
+                <div class="h4 pb-1">Download</div>
+                <p class="text-muted">Download from Play Store</p>
+              </div>
+            </div>
+          </div></a></div>
+
+    </div>
+  </div>
+</div>
+    </header>
+   
+     
+
+  <div class="page-content"></div>
+    <footer class="footer-black aa-footer">
+      <div class="container py-5">
+        <div class="row text-center">
+          <div class="col-md-12"><a class="btn btn-link btn-neutral" href="http://www.facebook.com/ANbilArabi"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a><a class="btn btn-link btn-neutral" href="http://twitter.com/ANbilArabi"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a><a class="btn btn-link btn-neutral" href="https://g.page/anbilarabi?share"><i class="fa fa-google-plus fa-2x" aria-hidden="true"></i></a><a class="btn btn-link btn-neutral" href="http://instagram.com/anbilarabi"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></div>
+          <div class="col-md-12">
+            <p class="mt-3">Copyright &copy; anbilarabi App. All rights reserved.<br>Designed by - <a class="credit" href="https://greenbackend.com" target="_blank">Greenbackend</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="<?php echo plugins_url('/js/jquery-3.2.1.min.js', __FILE__); ?>"></script>
+    <script src="<?php echo plugins_url('/js/jquery-ui-1.12.1.custom.min.js', __FILE__); ?>"></script>
+    <script src="<?php echo plugins_url('/js/popper.js', __FILE__); ?>"></script>
+    <script src="<?php echo plugins_url('/js/bootstrap.min.js', __FILE__); ?>"></script>
+    <script src="<?php echo plugins_url('/js/paper-kit.js?v=2.1.0', __FILE__); ?>"></script>
+    <script src="<?php echo plugins_url('/scripts/main.js', __FILE__); ?>"></script>
+  </body>
+</html>
